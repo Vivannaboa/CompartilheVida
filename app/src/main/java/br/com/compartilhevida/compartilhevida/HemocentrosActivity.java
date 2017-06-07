c64967193a6012272d2563e47ec01c5e80d47ef3
vivan <m.vivan1@gmail.com>
2017-06-07 14:57:54 -0300

    maps fim

--- app/src/main/java/br/com/compartilhevida/compartilhevida/HemocentrosActivity.java
+++ app/src/main/java/br/com/compartilhevida/compartilhevida/HemocentrosActivity.java
@@ -20,6 +20,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -42,93 +43,100 @@ public class HemocentrosActivity extends FragmentActivity implements
         OnMapReadyCallback,
         GoogleApiClient.OnConnectionFailedListener,
         GoogleApiClient.ConnectionCallbacks {
-    private static final String TAG = "HemocentroActivity";
+    private static final String TAG = "HemocentrosActivity";
     private static final int MY_PERMISSIONS_REQUEST_READ_LOCATION = 10;
     private GoogleMap mMap;
     private Location myLocation;
     private Location mLastLocation;
+    private LatLng mLocation = null;
     private String mLatitude, mLongitude;
     private DatabaseReference mDatabase;
+    public SupportMapFragment mapFragment;
     private List<Hemocentro> hemocentroList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hemocentros);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        // obtem o mapa para o fragmento e aguarda as notificações quando o mapa estiver pronto para ser usado.
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        //Criando um listner para buscad dados do banco
-        mDatabase.child("hemocentros").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                try {
-                hemocentroList.clear();
-                int i = 0;
-                for (DataSnapshot suggestionSnapshot : dataSnapshot.getChildren()) {
-                    Hemocentro hemocentro = suggestionSnapshot.getValue(Hemocentro.class);
-                    hemocentro.setUid(suggestionSnapshot.getKey());
-                    hemocentroList.add(hemocentro);
-                    Log.i(TAG, "banco: " + i++);
-                }
-                Log.i(TAG, "carregou os dados do banco");
-                adivionarMarcadores();
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-
-        });
-
-
-        // Create an instance of GoogleAPIClient.
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
+        //Cria uma instância do GoogleAPIClient
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
-        }
+
+        //pega a refêrencia do banco de dados
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        //Criando um listner para buscad dados do banco
+        //addListenerForSingleValueEvent esse listener busca apenas uma vez os dados, não fica observando se teve mudanças
+        mDatabase.child("hemocentros").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                try {
+                    hemocentroList.clear();
+                    for (DataSnapshot suggestionSnapshot : dataSnapshot.getChildren()) {
+                        //converte o json em objeto
+                        Hemocentro hemocentro = suggestionSnapshot.getValue(Hemocentro.class);
+                        hemocentro.setUid(suggestionSnapshot.getKey());
+                        //adiciona a lista
+                        hemocentroList.add(hemocentro);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Log.i(TAG, "carregou os dados do banco");
+                adicionarMarcadores();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.i(TAG, "O carregamento dos Dados foi cancelado");
+            }
+        });
 
     }
 
     @Override
     protected void onStart() {
         mGoogleApiClient.connect();
+        Log.i(TAG, "Passou no onStart");
         super.onStart();
     }
 
     @Override
     protected void onStop() {
         mGoogleApiClient.disconnect();
+        Log.i(TAG, "Passou no OnStop");
         super.onStop();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.i(TAG, "Terminou de carregar o mapa");
         mMap = googleMap;
+        //configura o mapa e pede permisão ao usuário para acessar a localização se ainda não possuir
         setUpMap();
     }
 
     public void setUpMap() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_READ_LOCATION);
+            Log.i(TAG, "Pediu permissão ao usuário");
             return;
         } else {
+            //habilita o botão para Mylocstion
             mMap.setMyLocationEnabled(true);
-//            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         }
     }
 
-
+    //pega o retorno da mensagem de permissão e se o usuário não consedeu permissão avisa que não vai conseguir proceguir
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
@@ -158,34 +166,42 @@ public class HemocentrosActivity extends FragmentActivity implements
     public void onConnected(@Nullable Bundle bundle) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_READ_LOCATION);
+            Log.i(TAG, "Pediu permissão ao usuário");
             return;
+
         }
+        //pega a localização atual
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if (mLastLocation != null) {
+            Log.i(TAG, "Adicionou a minha localização");
             mLatitude = String.valueOf(mLastLocation.getLatitude());
             mLongitude = String.valueOf(mLastLocation.getLongitude());
             // Add a marker in Sydney and move the camera
-            LatLng mLocation = new LatLng(Double.parseDouble(mLatitude), Double.parseDouble(mLongitude));
-            mMap.addMarker(new MarkerOptions().position(mLocation).title("Minha Localização"));
+            mLocation = new LatLng(Double.parseDouble(mLatitude), Double.parseDouble(mLongitude));
+            MarkerOptions mMarkerOptions = new MarkerOptions();
+            mMarkerOptions.position(mLocation);
+            mMarkerOptions.title("Minha Localização");
+            mMarkerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+            mMap.addMarker(mMarkerOptions);
 
             mMap.animateCamera(CameraUpdateFactory.newLatLng(mLocation));
 
-            // Zoom in, animating the camera.
+            // Zoom animando a câmera.
             mMap.animateCamera(CameraUpdateFactory.zoomIn());
 
-            // Zoom out to zoom level 10, animating with a duration of 2 seconds.
-            mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+            // Diminuindo o zoom e aumentado o nível para 10, animando com uma duração de 2 segundos.
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(30), 2000, null);
 
-            // Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
+            // contruindo uma CameraPosition com foco Mountain View.
             CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(mLocation)      // Sets the center of the map to Mountain View
-                    .zoom(5)                   // Sets the zoom
-                    .bearing(0)                // Sets the orientation of the camera to east
-                    .tilt(30)                   // Sets the tilt of the camera to 30 degrees
-                    .build();                   // Creates a CameraPosition from the builder
+                    .target(mLocation)      // Define o centro do mapa para Mountain View
+                    .bearing(10)                // Define a orientação da câmera para o leste
+                    .zoom(20)
+                    .tilt(5)                   // Define a inclinação da câmera para 30 graus
+                    .build();                   // chama o conrturor
+            //passa a posição para o mapa
             mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-            Log.i(TAG, "Adicinando os pinos no mapa");
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(7));
 
         }
     }
@@ -200,46 +216,59 @@ public class HemocentrosActivity extends FragmentActivity implements
 
     }
 
-    private void adivionarMarcadores() {
+    private void adicionarMarcadores() {
         LatLng mLocation2;
         try {
-            int i = 0;
+            //percore a lista de hemocentros
             for (Hemocentro hemocentro : hemocentroList) {
-                if (hemocentro.getLng()==0 && hemocentro.getLat()==0){
+                if (hemocentro.getLat() != 0.0 && hemocentro.getLng() != 0.0) {//se não tem lat/lng tem que procurar pelo endereços
+                    mLocation2 = new LatLng(hemocentro.getLat(), hemocentro.getLng());
+                } else {
                     mLocation2 = pegaCoordenadaDoEndereco(hemocentro.getEndereco());
-                }else{
-                    mLocation2 = new LatLng(hemocentro.getLat(),hemocentro.getLat());
-                    updateHemocentro(hemocentro);
+                    if (mLocation2 != null) {
+                        hemocentro.setLat(mLocation2.latitude);
+                        hemocentro.setLng(mLocation2.longitude);
+                        updateHemocentro(hemocentro);
+                    }
                 }
-                mMap.addMarker(new MarkerOptions().position(mLocation2).title(String.valueOf(i++)));
+                if (mLocation2 != null) {
+                    MarkerOptions markerOptions = new MarkerOptions();//cria uma nova configuração para o marcador
+                    markerOptions.position(mLocation2);//passa a posição
+                    markerOptions.title(hemocentro.getNome());//coloca o nome do ponto
+                    mMap.addMarker(markerOptions);
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(mLocation));//atualiza avisualização
+                }
             }
-
         } catch (Exception ex) {
             Log.d(TAG, ex.getMessage());
         }
+        Log.i(TAG, "Adicionou os hemocentros no mapa");
     }
 
-    private void updateHemocentro(Hemocentro hemocentro){
+    //atualiza os dados do hemocentro
+    private void updateHemocentro(Hemocentro hemocentro) {
         Map<String, Object> postValues = hemocentro.toMap();
         mDatabase.child("hemocentros").child(hemocentro.getUid()).updateChildren(postValues);
     }
 
+    //decolve as cordenadas de um endereço
     private LatLng pegaCoordenadaDoEndereco(String endereco) {
-        try{
+        try {
             Geocoder geocoder = new Geocoder(this);
 
             List<Address> resultados = geocoder.getFromLocationName(endereco, 1);
 
-            if(!resultados.isEmpty()){
+            if (!resultados.isEmpty()) {
                 LatLng posicao = new LatLng(resultados.get(0).getLatitude(),
                         resultados.get(0).getLongitude());
                 return posicao;
             }
 
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
+
 
 }
