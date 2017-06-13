c0db29f2b2c5bea9410ddad3335cd4820dca3311
DeboraEngelmann <debo.c.e.3@gmail.com>
2017-06-13 07:19:23 -0300

    tela de doações

--- app/src/main/java/br/com/compartilhevida/compartilhevida/DoacaoActivity.java
+++ app/src/main/java/br/com/compartilhevida/compartilhevida/DoacaoActivity.java
@@ -1,7 +1,12 @@
 package br.com.compartilhevida.compartilhevida;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ScrollingView;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 
 public class DoacaoActivity extends BaseActivity {
 
@@ -9,7 +14,19 @@ public class DoacaoActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doacao);
+        Toolbar tlb = (Toolbar) findViewById(R.id.toolbarDoacao);
+        setSupportActionBar(tlb);
         getSupportActionBar().setTitle("Registro de Doação" );
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_edit,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
